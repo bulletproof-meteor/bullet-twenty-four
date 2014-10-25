a34ca6b0dffc36c33cb4a6dffbd79a1baadbd8db
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-10-26 04:23:38 +0530

    using aggregations

--- server/main.js
+++ server/main.js
@@ -2,20 +2,20 @@ Meteor.methods({
   "getMetrics": function() {
     var resTime = {};
     var sevenDaysAgo = new Date(Date.now() - 1000 * 3600 * 24 * 7);
-    Metrics.find({time: {$gt: sevenDaysAgo}}).forEach(function(doc) {
-      var date = doc.time.getDate();
-      if(!resTime[date]) {
-        resTime[date] = {total: 0, count: 0};
-      }
 
-      resTime[date].total += doc.resTime;
-      resTime[date].count++;
+    var result = Metrics.aggregate([
+      {$match: {time: {$gt: sevenDaysAgo}}},
+      {$group: {
+        _id: {$dayOfMonth: "$time"},
+        avg: {$avg: "$resTime"}
+      }}
+    ]);
+
+    var resTime = {};
+    result.forEach(function(doc) {
+      resTime[doc._id] = {avg: doc.avg}
     });
-
-    for(var date in resTime) {
-      resTime[date].avg = resTime[date].total / resTime[date].count;
-    }
-
+    
     return resTime;
   }
 });
